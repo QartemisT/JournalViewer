90dc516f3c8349787d113b1b3154af6ac96fa2b5
QartemisT <QartemisT@gmail.com>
2020-09-13 17:51:08 +0100

    Cleaner code.... by far

--- js/index.js
+++ js/index.js
@@ -1,54 +1,57 @@
 let latestBuild, cacheStore;
-const newCache = {},
-	cache = {};
-
-const selectedBuild = "wow_beta"; // TODO: Allow changing
-
-const builds = {
-	"wow":		{
-		link: "",
-		name: "Live"
+const selectedBuild = "wow_beta", // TODO: Allow changing
+	newCache = {},
+	cache = {},
+	reqResponse = {},
+	reqHeaders = [
+		"spelleffect",
+		"spellmisc",
+		"spell"
+	],
+	builds = {
+		"wow":		{
+			link: "",
+			name: "Live"
+		},
+		"wowt":		{
+			link: "ptr.",
+			name: "PTR"
+		},
+		"wow_beta":	{
+			link: "shadowlands.",
+			name: "Beta"
+		},
+		"wowdev":	{
+			link: "",
+			name: "Alpha"
+		}
 	},
-	"wowt":		{
-		link: "ptr.",
-		name: "PTR"
-	},
-	"wow_beta":	{
-		link: "shadowlands.",
-		name: "Beta"
-	},
-	"wowdev":	{
-		link: "",
-		name: "Alpha"
-	}
-}
-
-const iconFlags = {
-	1:		"tank",
-	2:		"damage",
-	4:		"healer",
-	8:		"heroic",
-	16:		"deadly",
-	32:		"important",
-	64:		"interruptible",
-	128:	"magic",
-	256:	"curse",
-	512:	"poison",
-	1024:	"disease",
-	2048:	"enrage",
-	4096:	"mythic",
-}
+	iconFlags = {
+		1:		"tank",
+		2:		"damage",
+		4:		"healer",
+		8:		"heroic",
+		16:		"deadly",
+		32:		"important",
+		64:		"interruptible",
+		128:	"magic",
+		256:	"curse",
+		512:	"poison",
+		1024:	"disease",
+		2048:	"enrage",
+		4096:	"mythic",
+	};
 
 function sanityText(text) {
 	text = text.replace(/\$bullet;/g, "<br>&bull; "); // New line
 	text = text.replace(/\|cFF([a-z0-9]+)\|Hspell:([0-9]+)\s?\|h([^|]+)\|h\|r/gi, " <a style=\"color: #$1;\" href=\"https://" + builds[selectedBuild].link + "wowhead.com/spell=$2\" data-wowhead=\"spell-$2\">$3</a>"); // Spell tooltips
 	text = text.replace(/\$\[[0-9,]+(?:[\s\n]+)?(.*?)\$]/g, "$1"); // Ignored difficulty text
 	text = text.replace(/\$\[![0-9,]+(?:[\s\n]+)?(.*?)\$]/g, "<p class=\"iconsprite warning\">$1</p>"); // Difficulty warning text
-	text = text.replace(/\$(\d+)s(\d+)/g, function(_, spellID, section) { // SpellEffect variables
+	text = text.replace(/\$(\d+)s(\d+)/g, (_, spellID, section) => { // SpellEffect variables
 		let cacheIndex = spellID + "-" + (parseInt(section) - 1);
-		if(typeof(cache[cacheIndex]) === "undefined") {
+		if(!cache[cacheIndex]) {
 			let storeCacheIndex = cacheIndex;
-			if(typeof(newCache[cacheIndex]) === "undefined") {
+			if(!newCache[cacheIndex]) {
 				storeCacheIndex = spellID + "-0";
 			}
 			cacheStore.transaction(latestBuild, "readwrite").objectStore(latestBuild).add(newCache[storeCacheIndex], cacheIndex);
@@ -65,11 +68,9 @@ function elementIcons(bit) {
 		return "<li>";
 	}
 	let ret = "<li class=\"iconsprite ";
-	for(let bitKey of Object.keys(iconFlags)) {
-		if(bit & bitKey) {
-			ret += iconFlags[bitKey] + " ";
-		}
-	}
+	Object.keys(iconFlags)
+		.filter(bitKey => bit & bitKey)
+		.map(bitKey => ret += iconFlags[bitKey] + " ");
 	ret += "\">";
 	return ret;
 }
@@ -80,114 +81,103 @@ function initBossSections() {
 		download:		true,
 		delimiter:		",",
 		skipEmptyLines:	true,
-		complete:		function(data) {
+		complete: (data) => {
 			const store = {
 				Overview:	{},
 				Abilities:	{}
 			}
-			for(let i = 1; i < data.data.length; i++) {
-				const dat = data.data[i];
-				if(dat[8] === "3" && dat[13] !== "3") { // Overview
-					if(typeof(store.Overview[dat[3]]) === "undefined") {
-						store.Overview[dat[3]] = [];
+			data.data.shift();
+			data.data.map(data => {
+				if(data[8] === "3" && data[13] !== "3") { // Overview
+					if(!store.Overview[data[3]]) {
+						store.Overview[data[3]] = [];
 					}
-					store.Overview[dat[3]][dat[4]] = dat;
-				} else if(dat[9] === "0" || dat[8] === "1" || dat[8] === "2") { // Abilities: Header | Creature | spell
-					if(typeof(store.Abilities[dat[3]]) === "undefined") {
-						store.Abilities[dat[3]] = [];
+					store.Overview[data[3]][data[4]] = data;
+				} else if(data[9] === "0" || data[8] === "1" || data[8] === "2") { // Abilities: Header | Creature | spell
+					if(!store.Abilities[data[3]]) {
+						store.Abilities[data[3]] = [];
 					}
-					store.Abilities[dat[3]][dat[4]] = dat;
+					store.Abilities[data[3]][data[4]] = data;
 				}
-			}
-			let iter = Object.keys(store.Overview).concat(Object.keys(store.Abilities));
-			for(let encounterID of iter.filter((value, index) => iter.indexOf(value) === index)) {
-				const elem = document.querySelector("#boss-" + encounterID + " + label + div");
-				if(elem == null) {
-					continue;
-				}
-				for(let storeType of Object.keys(store)) {
-					const sectionStore = store[storeType];
-					if(typeof(sectionStore[encounterID]) !== "undefined") {
-						let contents = "<ul>",
-							prevParent = "0",
-							prevIndent = 0,
-							siblings = [];
-						for(let section of Object.values(sectionStore[encounterID])) {
-							if(section[5] === "0") {
-								siblings[section[0]] = 0;
-							} else {
-								siblings[section[0]] = siblings[section[5]] + 1;
-							}
-							if(prevParent !== section[5]) {
-								if(siblings[section[0]] < prevIndent) {
-									for(let i = 0; i < prevIndent - siblings[section[0]]; i++) {
-										contents += "</ul>";
-									}
+			});
+			Object.keys(store.Overview).concat(Object.keys(store.Abilities))
+				.filter((encounterID, index, self) => self.indexOf(encounterID) === index && document.querySelector("#boss-" + encounterID + " + label + div"))
+				.map(encounterID => {
+					const elem = document.querySelector("#boss-" + encounterID + " + label + div");
+					Object.keys(store)
+						.filter(storeType => store[storeType][encounterID])
+						.map(storeType => {
+							const sectionStore = store[storeType];
+							let contents = "<ul>",
+								prevParent = "0",
+								prevIndent = 0,
+								siblings = [];
+							Object.values(sectionStore[encounterID]).map(section => {
+								if(section[5] === "0") {
+									siblings[section[0]] = 0;
 								} else {
-									contents += "<ul>";
+									siblings[section[0]] = siblings[section[5]] + 1;
 								}
-							}
-							prevParent = section[5];
-							prevIndent = siblings[section[0]];
-							if(storeType === "Overview") {
-								contents += elementIcons(section[14]) + "<b>" + section[1] + "</b> " + sanityText(section[2]) + "</li>";
-							} else {
-								// TODO: SpellID
-								contents += elementIcons(section[14]) + "<b>" + section[1] + "</b> " + sanityText(section[2]) + "</li>";
-							}
-							prevParent = section[5];
-						}
-						contents += "</ul>";
-						elem.innerHTML += "\
-							<input id=\"boss-" + encounterID + "-" + storeType + "\" type=\"radio\" name=\"boss-" + encounterID + "\">\
-							<label for=\"boss-" + encounterID + "-" + storeType + "\">" + storeType + "</label>\
-							<div>\
-								" + contents + "\
-							</div>";
-					}
-				}
-				/*
-				elem.innerHTML += "\
-					<input id=\"boss-" + encounterID + "-loot\" type=\"radio\" name=\"boss-" + encounterID + "\">\
-					<label for=\"boss-" + encounterID + "-loot\">Loot</label>\
-					<div>\
-						" + contents + "\
-					</div>";
-				*/
-			}
+								if(prevParent !== section[5]) {
+									if(siblings[section[0]] < prevIndent) {
+										for(let i = 0; i < prevIndent - siblings[section[0]]; i++) {
+											contents += "</ul>";
+										}
+									} else {
+										contents += "<ul>";
+									}
+								}
+								prevParent = section[5];
+								prevIndent = siblings[section[0]];
+								if(storeType === "Overview") {
+									contents += elementIcons(section[14]) + "<b>" + section[1] + "</b> " + sanityText(section[2]) + "</li>";
+								} else {
+									// TODO: SpellID
+									contents += elementIcons(section[14]) + "<b>" + section[1] + "</b> " + sanityText(section[2]) + "</li>";
+								}
+								prevParent = section[5];
+							});
+							contents += "</ul>";
+							elem.innerHTML += "\
+								<input id=\"boss-" + encounterID + "-" + storeType + "\" type=\"radio\" name=\"boss-" + encounterID + "\">\
+								<label for=\"boss-" + encounterID + "-" + storeType + "\">" + storeType + "</label>\
+								<div>\
+									" + contents + "\
+								</div>";
+						});
+				});
 		}
 	});
 }
 
 function initBosses() {
 	// Parse bosses
-	const bosses = {}
 	Papa.parse("https://wow.tools/dbc/api/export/?name=journalencounter&build=" + latestBuild, {
 		download:		true,
 		delimiter:		",",
 		skipEmptyLines:	true,
-		complete:		function(data) {
-			for(let i = 1; i < data.data.length; i++) {
-				const dat = data.data[i];
-				if(dat[5] === "0") { // Fix for PvP Battlegrounds being listed.
-					continue;
-				}
-				if(typeof(bosses[dat[5]]) === "undefined") {
-					bosses[dat[5]] = [];
-				}
-				bosses[dat[5]].splice(dat[7], 0, dat);
-			}
-			for(let instanceID of Object.keys(bosses)) {
+		complete: (data) => {
+			const bosses = {}
+			data.data.shift();
+			data.data
+				.filter((data) => data[5] !== "0")
+				.map(data => {
+					if(!bosses[data[5]]) {
+						bosses[data[5]] = [];
+					}
+					bosses[data[5]].splice(data[7], 0, data);
+				});
+			Object.keys(bosses).map(instanceID => {
 				const elem = document.querySelector("#instance-" + instanceID + " + label + div");
-				for(let boss of Object.values(bosses[instanceID])) {
+				Object.values(bosses[instanceID]).map(boss => {
 					elem.innerHTML += "\
 					<input id=\"boss-" + boss[0] + "\" type=\"radio\" name=\"instance-" + boss[5] + "\">\
 					<label for=\"boss-" + boss[0] + "\">" + boss[1] + "</label>\
 					<div class=\"tabbed\">\
 						<div>" + boss[2] + "</div>\
 					</div>";
-				}
-			}
+				});
+			});
 			initBossSections();
 		}
 	});
@@ -199,27 +189,26 @@ function initInstances() {
 		download:		true,
 		delimiter:		",",
 		skipEmptyLines:	true,
-		complete:		function(data) {
-			for(let i = 1; i < data.data.length; i++) {
-				const dat = data.data[i];
-				document.querySelector("#expansion-" + dat[1] + " + label + div").innerHTML += "\
-				<input id=\"instance-" + dat[2] + "\" type=\"radio\" name=\"expansion-" + dat[1] + "\">\
-				<label for=\"instance-" + dat[2] + "\"></label>\
+		complete: (data) => {
+			data.data.shift();
+			data.data.map(data => {
+				document.querySelector("#expansion-" + data[1] + " + label + div").innerHTML += "\
+				<input id=\"instance-" + data[2] + "\" type=\"radio\" name=\"expansion-" + data[1] + "\">\
+				<label for=\"instance-" + data[2] + "\"></label>\
 				<div class=\"tabbed\"></div>";
-			}
+			});
 			Papa.parse("https://wow.tools/dbc/api/export/?name=journalinstance&build=" + latestBuild, {
 				download:		true,
 				delimiter:		",",
 				skipEmptyLines:	true,
-				complete:		function(data) {
-					for(let i = 1; i < data.data.length; i++) {
-						const dat	= data.data[i],
-							elem	= document.querySelector("#instance-" + dat[2] + " + label");
-						if(elem !== null) { // Fix for unassigned instances (e.g. 235: Scarlet Monastery - OLD, PH)
-							elem.innerHTML = dat[0];
-							document.querySelector("#instance-" + dat[2] + " + label + div").innerHTML += dat[1];
-						}
-					}
+				complete: (data) => {
+					data.data.shift();
+					data.data
+						.filter(data => document.querySelector("#instance-" + data[2] + " + label"))
+						.map(data => {
+							document.querySelector("#instance-" + data[2] + " + label").innerHTML = data[0];
+							document.querySelector("#instance-" + data[2] + " + label + div").innerHTML += data[1];
+						});
 					initBosses();
 				}
 			});
@@ -233,66 +222,48 @@ function initExpansions() {
 		download:		true,
 		delimiter:		",",
 		skipEmptyLines:	true,
-		complete:		function(data) {
-			const elem = document.getElementsByClassName('tabbed')[0];
-			let isFirst = true;
-			for(let i = data.data.length - 1; i > 0; i--) {
-				const dat = data.data[i];
+		complete: (data) => {
+			const elem = document.getElementsByClassName("tabbed")[0];
+			data.data.shift();
+			data.data.reverse().map((data, index) => {
 				elem.innerHTML += "\
-				<input id=\"expansion-" + dat[0] + "\" type=\"radio\" name=\"expansion\"" + (isFirst ? "checked" : "") + ">\
-				<label for=\"expansion-" + dat[0] + "\">" + dat[1] + "</label>\
+				<input id=\"expansion-" + data[0] + "\" type=\"radio\" name=\"expansion\"" + (index === 0 ? "checked" : "") + ">\
+				<label for=\"expansion-" + data[0] + "\">" + data[1] + "</label>\
 				<div class=\"tabbed\"></div>";
-				isFirst = false;
-			}
+			});
 			initInstances();
 		}
 	});
 }
 
 function initSpellEffects() {
-	const spellEffectsReq = new XMLHttpRequest()
-	spellEffectsReq.onreadystatechange = function() {
-		if(spellEffectsReq.readyState !== 4) {
-			return;
+	const spellHeaderSpellID			= reqHeaders.spelleffect.indexOf("SpellID"),
+		spellHeaderEffectIndex			= reqHeaders.spelleffect.indexOf("EffectIndex"),
+		spellHeaderEffectBasePointsF	= reqHeaders.spelleffect.indexOf("EffectBasePointsF");
+	Papa.parse("https://wow.tools/dbc/api/export/?name=spelleffect&build=" + latestBuild, {
+		download:		true,
+		delimiter:		",",
+		skipEmptyLines:	true,
+		complete: (data) => {
+			data.data.shift();
+			data.data.map(data => newCache[data[spellHeaderSpellID] + "-" + data[spellHeaderEffectIndex]] = data[spellHeaderEffectBasePointsF]);
+			initExpansions();
 		}
-		const data = JSON.parse(spellEffectsReq.responseText)
-		const spellHeaderSpellID			= data.headers.indexOf('SpellID'),
-			spellHeaderEffectIndex			= data.headers.indexOf('EffectIndex'),
-			spellHeaderEffectBasePointsF	= data.headers.indexOf('EffectBasePointsF');
-		Papa.parse("https://wow.tools/dbc/api/export/?name=spelleffect&build=" + latestBuild, {
-			download:		true,
-			delimiter:		",",
-			skipEmptyLines:	true,
-			complete:		function(data) {
-				for(let i = 1; i < data.data.length; i++) {
-					const dat = data.data[i];
-					newCache[dat[spellHeaderSpellID] + "-" + dat[spellHeaderEffectIndex]] = dat[spellHeaderEffectBasePointsF];
-				}
-				initExpansions();
-			}
-		});
-	}
-	spellEffectsReq.open("GET", "https://wow.tools/dbc/api/header/spelleffect/?build=" + latestBuild, true);
-	spellEffectsReq.send(null);
+	});
 }
 
-const buildsReq = new XMLHttpRequest()
-buildsReq.onreadystatechange = function() {
-	if(buildsReq.readyState !== 4) {
-		return;
-	}
-	latestBuild = JSON.parse(buildsReq.responseText)[selectedBuild]
-	const store = indexedDB.open('CacheDB', 1);
-	store.onupgradeneeded = function() {
+function initCache() {
+	const store = indexedDB.open("CacheDB", 1);
+	store.onupgradeneeded = () => {
 		if(store.result.objectStoreNames.contains(latestBuild)) {
 			store.result.deleteObjectStore(latestBuild);
 		}
 		store.result.createObjectStore(latestBuild);
 	};
-	store.onsuccess = function() {
+	store.onsuccess = () => {
 		cacheStore = store.result;
 		const request = cacheStore.transaction(latestBuild).objectStore(latestBuild).openCursor();
-		request.onsuccess = function(event) {
+		request.onsuccess = (event) => {
 			let cursor = event.target.result;
 			if(cursor) { // Getting results
 				cache[cursor.primaryKey] = cursor.value;
@@ -307,5 +278,13 @@ buildsReq.onreadystatechange = function() {
 		};
 	};
 }
-buildsReq.open("GET", "https://wow.tools/api.php?type=latestbuilds", true);
-buildsReq.send(null);
+
+fetch("https://wow.tools/api.php?type=latestbuilds")
+	.then(response => response.json())
+	.then(json => {
+		latestBuild = json[selectedBuild];
+		Promise.all(reqHeaders.map(header => fetch("https://wow.tools/dbc/api/header/" + header + "/?build=" + latestBuild)))
+			.then(responses => Promise.all(responses.map(response => response.json())))
+			.then(headers => headers.map((header, index) => reqResponse[reqHeaders[index]] = header.headers))
+			.then(() => initCache());
+	});
