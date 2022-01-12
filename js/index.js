2f35fc04a2692be6b00fc57a7a39d78c74b784ed
QartemisT <QartemisT@users.noreply.github.com>
2022-01-12 00:54:39 +0000

    deploy: 67691d757ce8dfac68be5224332876f4de7f26c5

--- js/index.js
+++ js/index.js
@@ -136,8 +136,15 @@ const sanityText = (cacheData, text, overrideSpellID, spellMultiplier) => {
 	let prevSpellID;
 	text = text.replace(/\$bullet;/gi, "<br>&bull; "); // New line
 	text = text.replace(/\|cFF([a-z0-9]+)\|Hspell:([0-9]+)\s?\|h([^|]+)\|h\|r/gi, " <a style=\"color: #$1;\" href=\"https://" + builds[selectedBuild].link + "wowhead.com/spell=$2\" data-wowhead=\"spell-$2\">$3</a>"); // Spell tooltips
-	text = text.replace(/\$\[[0-9, ]+(?:[\s\n]+)?(.*?)\$]/g, "$1"); // Ignored difficulty text
-	text = text.replace(/\$\[!([0-9, ]+)(?:[\s\n]+)?(.*?)\$]/g, (_, diffs, txt) => {
+	text = text.replace(/\$\[[0-9, ]+(?:[\s\n]+)?(.*?)\$\]/g, (_, diffs, txt) => { // Difficulty specific
+		for(const diff of diffs.split(",")) {
+			if(selectedDifficulty === "all" || difficulties[selectedDifficulty][diff.trim()]) {
+				return txt;
+			}
+		}
+		return "";
+	});
+	text = text.replace(/\$\[!([0-9, ]+)(?:[\s\n]+)?(.*?)\$\]/g, (_, diffs, txt) => { // Dificulty specific WARNING
 		for(const diff of diffs.split(",")) {
 			if(selectedDifficulty === "all" || difficulties[selectedDifficulty][diff.trim()]) {
 				return "<p class=\"iconsprite warning\">" + txt + "</p>";
@@ -188,7 +195,7 @@ const sanityText = (cacheData, text, overrideSpellID, spellMultiplier) => {
 			console.log("Failed SpellEffect", text);
 			return errorText;
 		}
-		if(data.Effect === 2) {
+		if(data.Effect === 2 || data.Effect === 6) {
 			return Math.round(Math.abs(spellMultiplier * data.EffectBasePointsF / 100)).toLocaleString();
 		}
 		return Math.abs(data.EffectBasePointsF).toLocaleString();
